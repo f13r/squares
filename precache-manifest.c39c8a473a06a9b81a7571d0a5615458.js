51ddf858a63da067115a4ada1241e4a48af45fe8
Slavinskyi <Viktor>
2020-02-03 12:28:02 +0200

    Updates

---
+++ precache-manifest.c39c8a473a06a9b81a7571d0a5615458.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28da76ed24f78e8d55f839d8ae0e5d33",
+    "url": "/squares/index.html"
+  },
+  {
+    "revision": "1754a9daecc60b110fcb",
+    "url": "/squares/static/js/2.8d5886e9.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/squares/static/js/2.8d5886e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7753df8e537071a10f5",
+    "url": "/squares/static/js/main.8847de3c.chunk.js"
+  },
+  {
+    "revision": "d0dcd4d8951b7420732f",
+    "url": "/squares/static/js/runtime-main.92ebbb48.js"
+  }
+]);
